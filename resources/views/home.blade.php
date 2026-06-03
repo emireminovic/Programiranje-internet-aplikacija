8ad44455cf92cd1329fd3e3101c8a01e1bf34ff7
emireminovic <emir.eminovic45@gmail.com>
2026-06-03 15:02:54 +0200

    fix: use vite for css in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookCut</title>
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 
