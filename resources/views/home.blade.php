77f851cc76c8980765ff02f4b865477a4d906888
emireminovic <emir.eminovic45@gmail.com>
2026-06-04 13:53:22 +0200

    fix: use asset css in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookCut</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 </head>
 <body>
 
